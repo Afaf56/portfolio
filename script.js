854e27c768c8a8fc5efc73ad3c0be8380510dae8
Afaf Suleiman <afaf56.kodehode@gmail.com>
2024-03-13 12:56:05 +0100

    portfolio v2

--- script.js
+++ script.js
@@ -42,23 +42,3 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.right = "-200px";
 }
-
-/contact form/;
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbyrQfRyz3soXVfhTHbf9yDOzTH_mQThS-vaH-44BC54CPpErZTFOrEdIR2hZjHM_MRF/exec";
-
-const form = document.forms["submit-to-google-sheet"];
-const msg = document.getElementById("msg");
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => {
-      msg.innerHTML = "Message sent successfully";
-      setTimeout(function () {
-        msg.innerHTML = "";
-      }, 5000);
-      form.reset();
-    })
-    .catch((error) => console.error("Error!", error.message));
-});
